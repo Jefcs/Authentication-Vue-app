b4159edc536cf40d50debbb4c131e411cdf11b1e
Jefcs <jeffersonc.silva@hotmail.com>
2019-11-12 18:22:34 -0300

    token

---
+++ src/store.js
@@ -0,0 +1,53 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import axios from './axios-auth';
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    idToken: null,
+    userId: null
+  },
+  mutations: {
+    authUser (state, userData) {
+      state.idToken = userData.token,
+      state.userId = userData.userId
+    }
+  },
+  actions: {
+    signup ({commit}, authData) {
+      axios.post('/signupNewUser?key=AIzaSyC6nvCqkUJ99FaExGSHfLffEAlyAzQAnwM', {
+        email: authData.email,
+        password: authData.password,
+        returnSecureToken: true
+      })
+        .then(res => {
+          console.log(res)
+          commit('authUser', {
+            token: res.data.idToken,
+            userId: res.data.localId
+          })
+        })
+        .catch(error => console.log(error))
+    },
+    login ({commit}, authData) {
+      axios.post('/verifyPassword?key=AIzaSyC6nvCqkUJ99FaExGSHfLffEAlyAzQAnwM', {
+        email: authData.email,
+        password: authData.password,
+        returnSecureToken: true
+      })
+      .then(res => {
+        console.log(res)
+        commit('authUser', {
+          token: res.data.idToken,
+          userId: res.data.localId
+        })
+      })
+      .catch(error => console.log(error))
+    }
+  },
+  getters: {
+
+  }
+})
